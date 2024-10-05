ba1df41da791cd2978bcd9a4f97c3e3b419c390a
AndersondaSilvaMendonca <103041513+AndersondaSilvaMendonca@users.noreply.github.com>
2024-10-05 16:38:25 -0400

    Update ConexaoBD.java

--- biblioteca/src/ConexaoBD.java
+++ biblioteca/src/ConexaoBD.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class ConexaoBD {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "sua senha do mysql aqui";
 
     public static Connection getConexao() {
         try {
